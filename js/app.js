92eb66922a862de1e0e57b5cf0e4bfd974dbdde1
LluisCompanyCabanellas <lluiscompanycaba@gmail.com>
2022-04-29 15:46:55 +0200

    29abril

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+
+
+
+//First work JavaScript//
 let menu = document.getElementById("cabecera-menu-mobile");
 let hamburger = document.getElementById("menu-hamburger-button")
 
@@ -10,7 +14,7 @@ if(hamburger){
 
 
 
-
+//Second work JavaScript//
 
 let minus = document.getElementById("minus");
 let plusMinusInput = document.getElementById("plus-minus-input");
@@ -28,6 +32,85 @@ minus.addEventListener("click", () =>{
         plusMinusInput.value = (parseInt(plusMinusInput.value) - 1);
     }
 });
+
+
+
+//Third work JavaScript//
+
+let tabs = document.querySelectorAll(".tab");
+
+if(tabs) {
+    tabs.forEach(tab =>{
+
+        tab.addEventListener("click", () => {
+
+            tabs.forEach(tab =>{
+                tab.classList.remove("active");
+            });
+
+            tab.classList.add("active");
+        });
+    });
+}
+
+
+
+
+
+var tabButton = document.getElementsByClassName('tab'),
+    tabContent = document.getElementsByClassName('tab-content');
+
+tabButton[0].classList.add('active');
+tabContent[0].style.display = 'block';
+
+function  {
+     
+        tabContent[i].style.display = 'none';
+        tabButton[i].classList.remove('active');
+    }
+    document.getElementById(x).style.display = 'block';
+    currentTarget.classList.add('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var tabButton = document.getElementsByClassName('tab'),
+    tabContent = document.getElementsByClassName('tab-content');
+
+tabButton[0].classList.add('active');
+tabContent[0].style.display = 'block';
+
+function city(e, x) {
+    var i;
+    for (i = 0; i < tabButton.length; i++) {
+        tabContent[i].style.display = 'none';
+        tabButton[i].classList.remove('active');
+    }
+    document.getElementById(x).style.display = 'block';
+    e.currentTarget.classList.add('active');
+}
 
 
 
