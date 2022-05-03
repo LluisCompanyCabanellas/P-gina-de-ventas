8a17d85cbeaa283e5b7758e120518d6ef10aa2a9
LluisCompanyCabanellas <lluiscompanycaba@gmail.com>
2022-05-03 16:01:02 +0200

    3 mayo

--- js/modulos/boton-sumar-restar.js
+++ js/modulos/boton-sumar-restar.js
@@ -1,30 +1,32 @@
+
 export let botonSumarRestar = () => {
 
-    let minuses = document.getElementsByClassName("minus");
-    let plusMinusInput = document.getElementsByClassName("plus-minus-input");
-    let pluses = document.getElementsByClassName("plus");
+ 
+    let pluses = document.querySelectorAll(".plus");
+    let minuses = document.querySelectorAll(".minus");
 
     pluses.forEach(plus => {
-
         plus.addEventListener("click", () => {
     
-            plusMinusInput.value = (parseInt(plusMinusInput.value) + 1);
+            console.log(plus.parentNode.querySelector('.'));
+
+            let father = plus.parentNode;
+            parseInt(father.querySelector()+1)
+            
         });
     });
-        
-    minuses.forEach(minus => {
-
-        minus.addEventListener("click", () =>{
-        
-            if(plusMinusInput.value > 1) {
-                plusMinusInput.value = (parseInt(plusMinusInput.value) - 1);
-            }
-        });
         
     
+
+    minuses.forEach(minus=> {
+
+        minus.addEventListener("click", () => {
+
+            console.log(minus.parentNode.querySelector(''));
+
+            .value = (parseInt(.value) -1)
+
+
+        });
     });
 }
-
-
-
-
